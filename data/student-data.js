d60ccd77197663bf7d31113a4aa4823d64e310e7
zgildo01 <zzaragozagildo@gmail.com>
2022-09-19 17:19:35 -0400

    set up student array

---
+++ data/student-data.js
@@ -0,0 +1,8 @@
+const student = [
+  {name: 'Amanda', position: 'student'},
+  {name: 'Ana', position: 'student'},
+  {name: 'Anchi', position: 'student'},
+]
+export {
+  student
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+// import modules
+import express from 'express';
+
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+
+
+
+// Mount Middleware (app.use)
+
+
+
+// Mount routes
+
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
